98970468531b357019d5d0739b6be7f78949143f
TanmayJDesai <65262891+TanmayJDesai@users.noreply.github.com>
2022-01-24 22:56:38 -0800

    Update Map.cpp

--- Map.cpp
+++ Map.cpp
@@ -11,6 +11,25 @@ Map::Map()
 
 Map::Map(const Map &cpyFrom)
 {
+
+    m_head = nullptr;
+    KeyType m_keyForCpy;
+    ValueType m_valueForCpy;
+    m_tail = nullptr;
+
+    m_items = 0;
+    int i = 0;
+ 
+    while (i < cpyFrom.size())
+    {
+      
+        cpyFrom.get(i, m_keyForCpy, m_valueForCpy);
+        insert(m_keyForCpy, m_valueForCpy);
+        i++;
+    }
+    //add the nodes of anotherMap into current Linked List
+ 
+    /*
     // Edge case if the head is a nullptr it is an empty linked list so initalizing it would just be setting head and tail to nullptr and size to 0.
     if (cpyFrom.m_head == nullptr)
     {
@@ -22,40 +41,62 @@ Map::Map(const Map &cpyFrom)
     m_head               = new Node;
     m_head -> m_prev     = nullptr;
     Node * cpyInto = m_head;
-    //Created a temp variable and set the pointer to the first node of 
+    //Created a temp variable and set the pointer to the m_head which points to the new node.
     for (Node * point = cpyFrom.m_head; point -> m_next != nullptr; point = point -> m_next)
     {
+        //Loop through the linked list past in the parameter and copy over that node into the one that we make.
         Node * nextPoint       = new Node;
+        //Create a pointer to the node that we are "updating (it basically points to the node that we are storing the value in so that we can set that equal to the m_next of cpyInto.
         cpyInto -> m_keyType   = point -> m_keyType;
+        //copy the key from the point to the cpyInto
         cpyInto -> m_valueType = point -> m_valueType;
+        //Copy the value from the point to the cpyInto.
         cpyInto -> m_next      = nextPoint;
+        //When we saved the nextpoint pointer equal to the new node set the cpyInto's next pointer to the that pointer.
         nextPoint -> m_prev    = cpyInto;
+        //Nextpoints previous should now be cpyInto so its next can be the next node in the linked list.
         cpyInto                = nextPoint;
+        //cpyInto  set equal to nextPoint because it points to the next element of the linked list.
+        delete nextPoint;
     }
+    
+    //setting the last node of the linked list
     cpyInto -> m_keyType   = cpyFrom.m_tail -> m_keyType;
+    //Last node of linked list, its key = cpyFrom (parameter Map)'s key.
     cpyInto -> m_valueType = cpyFrom.m_tail -> m_valueType;
+    //Last node of linked list, its value = cpyFrom (parameter Map)'s value.
     cpyInto -> m_next      = nullptr;
+    //set the tail equal to the nullptr.
     m_tail                 = cpyInto;
+    */
     
 }
 
 Map & Map::operator = (const Map & cpyFrom)
 {
+    //Just did what Prof. Smallberg implemented in his HW1 solutions but for linked list.
+    //created a temp map and copied into it the parameter and then swapped it.
     if (this != &cpyFrom)
     {
         Map m_temp(cpyFrom);
         swap(m_temp);
     }
+    
     return *this;
 }
 
 Map::~Map()
 {
+    //Destructor
     if (m_head == nullptr)
     {
+
+        //If the head is a nullptr the linked list is empty so return.
         return;
     }
+    //created a pointed to the m_head of the linked list.
     Node * point = m_head;
+    //while the pointer isn't a nullptr create a new pointer and set that equal to the m_head's next or old pointer's next. Delete the old point thus making sure no memory leaks occur and set the old point = the next pointer.
     while (point != nullptr)
     {
         Node * new_Point = point -> m_next;
@@ -81,7 +122,7 @@ int Map::size() const
     // Return the number of key/value pairs in the map.
 }
 
-bool Map::insert(const std::string& key, const double& value)
+bool Map::insert(const KeyType& key, const ValueType& value)
 {
     bool isTrue = true;
     if (contains(key))
@@ -90,7 +131,6 @@ bool Map::insert(const std::string& key, const double& value)
     }
     else
     {
-        
         Node * new_Guy         = new Node;
         new_Guy -> m_keyType   = key;
         new_Guy -> m_valueType = value;
@@ -107,9 +147,12 @@ bool Map::insert(const std::string& key, const double& value)
             m_tail -> m_next = new_Guy;
             m_tail           = new_Guy;
             m_items++;
+
         }
+
         isTrue = true;
     }
+    
     // If key is not equal to any key currently in the map, and if the
     // key/value pair can be added to the map, then do so and return true.
     // Otherwise, make no change to the map and return false (indicating
@@ -120,7 +163,7 @@ bool Map::insert(const std::string& key, const double& value)
 }
 
 
-bool Map::update(const std::string& key, const double& value)
+bool Map::update(const KeyType& key, const ValueType& value)
 {
     for (Node * point = m_head; point != nullptr; point = point -> m_next)
     {
@@ -138,7 +181,7 @@ bool Map::update(const std::string& key, const double& value)
 }
 
 
-bool Map::insertOrUpdate(const std::string& key, const double& value)
+bool Map::insertOrUpdate(const KeyType& key, const ValueType& value)
 {
     if (update(key, value) == true)
     {
@@ -156,45 +199,74 @@ bool Map::insertOrUpdate(const std::string& key, const double& value)
 }
 
 
-bool Map::erase(const std::string& key)
+bool Map::erase(const KeyType& key)
 {
     if (!contains(key))
     {
         return false;
     }
+    
     Node * point;
-    for (point = m_head; point != nullptr; point = point -> m_next)
+    bool isTrue = true;
+    
+    if (m_head -> m_keyType == key && size() == 1)
     {
-        if (point -> m_keyType == key)
+        delete m_head;
+        m_head = nullptr;
+        m_tail  = nullptr;
+        m_items--;
+        return isTrue;
+    }
+    
+    if (m_tail -> m_keyType == key)
+    {
+        point = m_tail;
+        m_tail -> m_prev -> m_next = point -> m_next;
+        m_tail = point -> m_prev;
+        delete point;
+        m_items --;
+        return isTrue;
+    }
+ 
+    if (m_head -> m_keyType == key)
+    {
+        point = m_head;
+        m_head -> m_next -> m_prev = point -> m_prev;
+        m_head = point -> m_next;
+        delete point;
+        m_items--;
+        return isTrue;
+    }
+    
+    Node * newPoint;
+    
+    for ( newPoint = m_head; newPoint != nullptr ; newPoint = newPoint -> m_next)
+    {
+        if (newPoint -> m_next != nullptr && newPoint -> m_next ->m_keyType == key)
         {
-            if (point -> m_next != nullptr)
-            {
-                point -> m_next -> m_prev = point -> m_prev;
-            }
-            else
-            {
-                m_tail = point -> m_prev;
-            }
-            if (point -> m_prev != nullptr)
-            {
-                point -> m_prev -> m_next = point -> m_next;
-            }
-            else
-            {
-                m_head = point -> m_next;
-            }
+            break;
         }
     }
-    m_items--;
-    delete point;
-    return true;
+    
+    if (newPoint != nullptr)
+    {
+        point = newPoint -> m_next;
+        newPoint -> m_next = point -> m_next;
+        point -> m_next -> m_prev = point -> m_prev;
+        delete point;
+        m_items--;
+        return isTrue;
+    }
+    isTrue = false;
+
     // If key is equal to a key currently in the map, remove the key/value
     // pair with that key from the map and return true.  Otherwise, make
     // no change to the map and return false.
+    return isTrue;
 }
 
  
-bool Map::contains(const std::string& key) const
+bool Map::contains(const KeyType& key) const
 {
     // Return true if key is equal to a key currently in the map, otherwise
     // false.
@@ -209,7 +281,7 @@ bool Map::contains(const std::string& key) const
 }
  
  
-bool Map::get(const std::string& key, double& value) const
+bool Map::get(const KeyType& key, ValueType& value) const
 {
     for (Node * point = m_head; point != nullptr; point = point -> m_next)
     {
@@ -226,7 +298,7 @@ bool Map::get(const std::string& key, double& value) const
     return false; 
 }
 
-bool Map::get(int i, std::string& key, double& value) const
+bool Map::get(int i, KeyType& key, ValueType& value) const
 {
     //Basically just did what I did in HW but changed the loops so they incremeneted the pointer.
     Node * firstPoint = m_head;
